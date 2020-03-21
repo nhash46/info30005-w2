e2fc80b89f314e7fff3abe572b4925469e750405
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-03-21 13:55:39 +1100

    Update ws3js_working.js

--- ws3js_working.js
+++ ws3js_working.js
@@ -3,24 +3,38 @@ let guess = window.guess;
 let gameState = {	
   correctGuesses : [],   // may be use to store info relating to correct guesses, e.g. letters or indexes 
   incorrectGuesses : [], // store incorrect letters
-  remainingGuesses : 10  // keep track of number of attempts
+  remainingGuesses : 10,  // keep track of number of attempts
 };
 
 function setWord () {
- word = document.getElementById('word').value;
- if(word.length < 15){
-  alert("Word set to: " + word);
- }
- else{
-   alert("ERROR: " + word + " is tooooo long");
-   word = window.word;
- }
+
+  word = prompt("Enter a word to be guessed: ");
+  
+  if(word.length > 0 && word.length < 15){
+    alert("Word set to: " + word);
+  }
+  else{
+    alert("ERROR: " + word + " is tooooo long");
+    word = window.word;
+  }
  
+}
+
+function setTiles (){
+
+  var wordLength = word.length;
+
+  for(var i = 0; i< wordLength; i++){
+    gameState.correctGuesses[i] = "-";
+  }
+
+  document.getElementById("currentGuess").innerText = "hello";
+
 }
 
 
 function validate () {
-  guess = document.getElementById('geuss').value;
+  guess = document.getElementById('guess').value;
   if(isNaN(guess)){
     check(guess);
   }
