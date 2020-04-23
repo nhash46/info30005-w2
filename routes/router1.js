c7ecbd3d18cfeca02f20023f0f75d24b94ae338e
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-04-23 20:08:17 +1000

    Revert "renaming of routes, controller and model"
    This reverts commit d9641589c1dfbd38f93805005f16e82fd7a8262e.

---
+++ routes/router1.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+// create router
+const router1 = express.Router();
+
+const controller1 = require("../controllers/controller1.js");
+
+// empty path = retrieve all authors 
+router1.get("/", controller1.getAllForumPosts);
+
+module.exports = router1;
