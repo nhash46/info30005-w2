8aa55645143979eaa82d407ed5c004ddfe9a86b7
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-04-23 22:45:01 +1000

    bodyParser included

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 require("./models");
+
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET home page
 app.get("/", (req, res) => {
