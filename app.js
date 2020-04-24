4d1988061816755828b0954edca71ad867994aab
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-04-24 14:08:45 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 require("./models");
+
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET home page
 app.get("/", (req, res) => {
@@ -11,10 +17,14 @@ app.get("/", (req, res) => {
   });
 
 // Routes
-const router1 = require("./routes/router1");
+const forumRouter = require("./routes/forumRouter");
+const userRouter = require("./routes/userRouter");
 
-// auther routes handles by router1
-app.use("/forum-posts", router1);
+// auther routes handles by forumRouter
+app.use("/forum-posts", forumRouter);
+
+// user routes handled by userRouter
+app.use("/user", userRouter);
   
 // start app and listen for incoming requests on port
 app.listen(process.env.PORT || 3000, () => {
