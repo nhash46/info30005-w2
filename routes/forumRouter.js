eecd89e244d9f5e61ac3b1a5fa75f9fb6190c2b9
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-04-25 18:17:05 +1000

    Update forumRouter.js

--- routes/forumRouter.js
+++ routes/forumRouter.js
@@ -12,7 +12,7 @@ forumRouter.get('/', forumController.getAllForumPosts);
 forumRouter.post('/',forumController.addforum);
 
 //commment on particular post
-forumRouter.post('/comment/:_id',forumController.addComment);
+forumRouter.post('/comment/',forumController.addComment);
 
 //search post by id
 forumRouter.get('/:_id',forumController.getforumByID);
