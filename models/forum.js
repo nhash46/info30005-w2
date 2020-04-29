2f7ea5d96fecb82c0d4e21ad96028bed76a53603
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-04-29 17:06:57 +1000

    try populate a single comment

--- models/forum.js
+++ models/forum.js
@@ -5,14 +5,10 @@ const Schema = mongoose.Schema;
 const forumPostSchema = new mongoose.Schema({
   title: String,
   body: String,
-  comments : [
-    {
-      comment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-      }
-    }
-  ] 
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
 });
 
 const Post = mongoose.model("Post", forumPostSchema, "forum_posts");
