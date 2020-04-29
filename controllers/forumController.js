305fae98b16f6f83feb8ac67da5e7415c5ccb940
Daniel Coleman <dccol@student.unimelb.edu.au>
2020-04-29 16:47:00 +1000

    trying dif population methods

--- controllers/forumController.js
+++ controllers/forumController.js
@@ -20,14 +20,14 @@ const addforum = (req, res) => {
     
 // function to handle a request to get all forums
 const getAllForumPosts = async (req, res) => {
-    
-  try {
-    const all_posts = await Post.find().populate("commentss");
-    return res.send(all_posts);
-  } catch (err) {
-    res.status(400);
-    return res.send("Database query failed");
-  }
+  
+  const all_posts = await Post.find().populate("comments").exec(function(err, forum_posts) {
+    if (err)
+      res.send(err);
+
+    res.json(av);
+  return res.send(all_posts);
+  })
 };
 
 
