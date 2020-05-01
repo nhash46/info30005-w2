a8db6592ea4c0b24905efb7585215290f7c0e9b5
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-05-01 12:28:59 +1000

    made title and body fields in forum required

--- models/forum.js
+++ models/forum.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const forumPostSchema = new mongoose.Schema({
-  title: String,
-  body: String,
+  title: {type: String, required: true},
+  body: {type: String, required: true},
   comments: [{
     type: Schema.Types.ObjectId,
     ref: "Comment"
