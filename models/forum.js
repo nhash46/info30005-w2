9d2ced303183cc9b821c89b75eb370da8d553af8
JaredM-Unimelb <61996178+JaredM-Unimelb@users.noreply.github.com>
2020-05-14 15:06:26 +1000

    Revert "Populate and model fix?"
    This reverts commit c4a9fdf68d3b894ea86c1fafc39292104b353831.

--- models/forum.js
+++ models/forum.js
@@ -4,14 +4,10 @@ const forumPostSchema = new mongoose.Schema({
   _id: Schema.Types.ObjectId,
   title: String,
   body: String,
-  comments : [
-    {
-      comment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-      }
-    }
-  ]
+  comments : [{
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }]
 });
 
 const Post = mongoose.model("Post", forumPostSchema, "forum_posts");
