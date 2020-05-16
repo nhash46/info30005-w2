61eb164c129b3de273f1ae17210558a62bc8f2dc
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-05-16 19:03:47 +1000

    Revert "backend for editing, delete comments"
    This reverts commit 413394184db9181a7f39e83ed69e88a5f8eea35f.

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const commentValidator = require("../validators/commentValidator.js");
 
 // create router
 const commentRouter = express.Router();
@@ -7,19 +6,10 @@ const commentRouter = express.Router();
 const commentController = require("../controllers/commentController.js");
 
 //commment on particular post
-commentRouter.post('/:_id', commentValidator.addComment, commentController.addComment);
+commentRouter.post('/:_id', commentController.addComment);
 
 commentRouter.get('/', commentController.getAllComments);
 
 commentRouter.get('/:title', commentController.getCommentByTitle);
-
-//edit form post by id
-commentRouter.get('/edit/:_id', commentController.ensureAuthenticated, commentController.editComment);
-
-//update forum post by id
-commentRouter.post('/edit/:_id' , commentController.updateComment);
-
-// delete forum post by id
-commentRouter.delete('/:_id', commentController.deleteComment);
 
 module.exports = commentRouter;
