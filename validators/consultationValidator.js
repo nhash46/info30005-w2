c172bad18e458401dd8e7580c02d9a27fb14a890
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-06-02 23:06:11 +1000

    date validator not working
    - taken out for the time being

--- validators/consultationValidator.js
+++ validators/consultationValidator.js
@@ -4,6 +4,6 @@ const {check} = require('express-validator/check');
 // validates that fields arent left empty
 exports.newConsultation = 
 [check('date').isLength({min:1}).trim().withMessage('Date required'),
-check('date').isAfter(Date.now().toString()).withMessage('Invalid Date selected, please select a future date'),
+// check('date').isAfter(Date.now().toString()).withMessage('Invalid Date selected, please select a future date'),
 check('time').isLength({min:1}).trim().withMessage('Time required'),
 check('venue').isLength({min:1}).trim().withMessage('Venue required')];
