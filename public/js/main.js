09c44528dbc2aea7ee68990191d01d8f5efc3cdd
nhash46 <61870054+nhash46@users.noreply.github.com>
2020-06-02 10:38:10 +1000

    consultation date validator
    - cannot enter past dates now

--- public/js/main.js
+++ public/js/main.js
@@ -87,6 +87,18 @@ $(document).ready(function(){
 });
 
 // ensures that a date in the past cannot be selected
-$(function () {
-    $(".date").date({ minDate: 0 })
+$(function(){
+    var dtToday = new Date();
+    
+    var month = dtToday.getMonth() + 1;
+    var day = dtToday.getDate();
+    var year = dtToday.getFullYear();
+    if(month < 10)
+        month = '0' + month.toString();
+    if(day < 10)
+        day = '0' + day.toString();
+    
+    var minDate= year + '-' + month + '-' + day;
+    
+    $('#txtDate').attr('min', minDate);
 });
